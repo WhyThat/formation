934ba2fa6457896a986523a5a2d103f71f674216
Mathieu HATON <mathieu.haton@link-value.fr>
2018-11-22 22:34:23 +0100

    feat: add selectGrid and selectCurrentPlayer in appState's selector

--- src/Redux/appState/selectors.js
+++ src/Redux/appState/selectors.js
@@ -1,4 +1,6 @@
 import { flatten } from 'ramda';
 
 export const selectPlayers = (state) => state.appState.players;
+export const selectGrid = (state) => state.appState.game.grid;
 export const selectFlattenedGrid = (state) => flatten(state.appState.game.grid);
+export const selectCurrentPlayer = (state) => state.appState.game.playerPlayingId;
