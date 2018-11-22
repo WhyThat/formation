260569d9c6b90f6ecba9fcfed8cca1b7ea10b99b
Mathieu HATON <mathieu.haton@link-value.fr>
2018-11-22 18:26:10 +0100

    feat: adding initialState to appState/players

--- src/Redux/appState/players/reducer.js
+++ src/Redux/appState/players/reducer.js
@@ -5,6 +5,14 @@ export const  playersActions = {
 }
 
 const initialState = {
+  player1: {
+    name: 'Player 1',
+    score: 0,
+  },
+  player2: {
+    name: 'Player 2',
+    score: 0,
+  },
 }
 
 export const playersReducer = (state) => {
