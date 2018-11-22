4e75aab21eb9e48c61251224ba6a5410c1e78899
Mathieu HATON <mathieu.haton@link-value.fr>
2018-11-22 15:26:03 +0100

    feat: adding rootSaga

--- src/Redux/createStore.js
+++ src/Redux/createStore.js
@@ -1,12 +1,14 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
 import { rootReducer } from './reducers';
+import { rootSaga } from './rootSaga';
 
-
+const sagaMiddleware = createSagaMiddleware()
 
 export const store = createStore(
   rootReducer,
-  composeWithDevTools(),
+  composeWithDevTools(applyMiddleware(sagaMiddleware)),
 );
 
-
+sagaMiddleware.run(rootSaga);
