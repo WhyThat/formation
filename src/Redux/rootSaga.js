b308fc5d1c0539b7be04700394f4238886142917
Mathieu HATON <mathieu.haton@link-value.fr>
2018-11-22 22:43:04 +0100

    feat: fork game tick's saga from root reducer

--- src/Redux/rootSaga.js
+++ src/Redux/rootSaga.js
@@ -1,7 +1,9 @@
 import { fork, put } from 'redux-saga/effects';
+import { gameTick } from './sagas/gameTick';
 import { initializeScenes } from './sagas/initializeScenes';
 
 export function* rootSaga() {
   yield put({type: 'SAGA_INITIALIZED'})
   yield fork(initializeScenes);
+  yield fork(gameTick);
 }
